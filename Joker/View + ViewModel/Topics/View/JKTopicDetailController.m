59936cd5980a71ae499b3856d7c2a7714fd5c999
朱彦君 <zhuyanjun@zhuyanjundeMacBook-Pro-2.local>
2017-08-08 15:54:28 +0800

    webview

--- Joker/View + ViewModel/Topics/View/JKTopicDetailController.m
+++ Joker/View + ViewModel/Topics/View/JKTopicDetailController.m
@@ -73,7 +73,7 @@
 - (void)webViewDidFinishLoad:(UIWebView *)webView{
     
     self.webView.scrollView.scrollEnabled = NO;
-    double delayInSeconds = 0.1;
+    double delayInSeconds = 0.5;
      dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
     dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
 //
